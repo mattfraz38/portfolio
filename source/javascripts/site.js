c6356b3205543c8a48f3bbef7698f62e58ee8338
Matt Frazier <frazier3388@yahoo.com>
2019-07-03 23:24:08 +0900

    add error message timeout

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -11,27 +11,30 @@ $(document).ready(function() {
     var message = $('.message').val()
     var statusElem = $('.status')
     statusElem.empty()
-
+// ADD SUCCESS MESSAGE IF SENT
     if(email.includes('@') && email.includes('.')) {
-      statusElem.append('<div>Email valid</div>')
+      // statusElem.append('<div class="error-msg">Invalid Email</div>')
     } else {
-      statusElem.append('<div>Invalid email</div>')
+      statusElem.append('<div class="error-msg">Invalid email</div>')
     };
 
-    if(subject.null) {
-      statusElem.append('<div>Subject is valid</div>')
-    } else {
-      statusElem.append('<div>Subject cannot be empty</div>')
+    if(subject.length < 1) {
+      statusElem.append('<div class="error-msg">Subject cannot be empty</div>')
+    // } else {
+    //   statusElem.append('<div class="error-msg">Subject cannot be empty</div>')
     };
 
-    if(message.length > 50) {
-      statusElem.append('<div>Message is valid')
-    } else {
-      statusElem.append('<div>Message is too short')
-    }
-  })
-})
+    if(message.length < 50) {
+      statusElem.append('<div class="error-msg">Message is too short')
+    // } else {
+    //   statusElem.append('<div class="error-msg">Message is too short')
+    };
 
+    setTimeout(function() {
+      $('.error-msg').remove();
+    }, 20000);
+  });
+});
 
 // const button = document.querySelector('.contact-button');
 //
