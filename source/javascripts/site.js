4ebc291894d574f4f8b8e421a96ee01bce80db07
Matt Frazier <frazier3388@yahoo.com>
2019-07-03 10:24:08 +0900

    add initial working js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,48 @@
 // This is where it all goes :)
+console.log('JS is working!')
+
+// $(document).ready(function () {
+//   $('.contact-button')click(function (event)) {
+//     event.preventDefault()
+//     console.log('Button clicked')
+//   }
+// })
+
+
+const button = document.querySelector('.contact-button');
+
+button.addEventListener('click', function() {
+  event.preventDefault();
+  console.log('Button clicked');
+});
+
+// button.addEventListener('click', event => {
+  //   event.preventDefault();
+  //   console.log('Button clicked');
+  // });
+
+
+// const button = document.getElementById('input');
+//
+// if(button) {
+//   button.addEventListener('click', function(event) {
+//     // event.preventDefault();
+//     console.log('Button clicked');
+//   });
+// }
+
+
+// const button = document.getElementById('h1');
+
+// if(document.getElementById("status") != null){
+  // console.log(button.innerHTML);
+// }
+
+// button.addEventListener('click', confirmationText);
+
+// if(button) {
+//   function confirmationText(event) {
+//     event.preventDefault();
+//     console.log('Button clicked');
+//   }
+// }
