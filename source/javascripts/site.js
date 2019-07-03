db09647a462c746708dd522d2128f94099ee4dfb
Matt Frazier <frazier3388@yahoo.com>
2019-07-03 13:01:56 +0900

    jquery validations added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,48 +1,67 @@
 // This is where it all goes :)
 console.log('JS is working!')
 
-// $(document).ready(function () {
-//   $('.contact-button')click(function (event)) {
-//     event.preventDefault()
-//     console.log('Button clicked')
-//   }
-// })
+$(document).ready(function() {
+  $('.contact-button').click(function(event) {
+    event.preventDefault()
+    console.log('Button clicked')
+
+    var email = $('.email').val()
+    var subject = $('.subject').val()
+    var message = $('.message').val()
+    var statusElem = $('.status')
+    statusElem.empty()
+
+    if(email.includes('@') && email.includes('.')) {
+      statusElem.append('<div>Email valid</div>')
+    } else {
+      statusElem.append('<div>Invalid email</div>')
+    };
+
+    if(subject.null) {
+      statusElem.append('<div>Subject is valid</div>')
+    } else {
+      statusElem.append('<div>Subject cannot be empty</div>')
+    };
+
+    if(message.length > 50) {
+      statusElem.append('<div>Message is valid')
+    } else {
+      statusElem.append('<div>Message is too short')
+    }
+  })
+})
 
 
-const button = document.querySelector('.contact-button');
-
-button.addEventListener('click', function() {
-  event.preventDefault();
-  console.log('Button clicked');
-});
-
-// button.addEventListener('click', event => {
-  //   event.preventDefault();
-  //   console.log('Button clicked');
-  // });
-
-
-// const button = document.getElementById('input');
+// const button = document.querySelector('.contact-button');
 //
-// if(button) {
-//   button.addEventListener('click', function(event) {
-//     // event.preventDefault();
-//     console.log('Button clicked');
-//   });
-// }
+// button.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   console.log('Button clicked');
 
+  // const email = ('.email').value;
+  // const subject = ('.subject').value;
+  // const message = ('.message').value;
+  // const statusElem = function {
+  //   ('.status');
+  // };
+  // statusElem.remove();
 
-// const button = document.getElementById('h1');
-
-// if(document.getElementById("status") != null){
-  // console.log(button.innerHTML);
-// }
-
-// button.addEventListener('click', confirmationText);
-
-// if(button) {
-//   function confirmationText(event) {
-//     event.preventDefault();
-//     console.log('Button clicked');
-//   }
-// }
+//   if(email.includes('@') && email.includes('.')) {
+//     statusElem.append('<div>Email valid</div>')
+//   } else {
+//     statusElem.append('<div>Invalid email</div>')
+//   };
+//
+//   if(subject.null) {
+//     statusElem.append('<div>Subject is valid</div>')
+//   } else {
+//     statusElem.append('<div>Subject cannot be empty</div>')
+//   };
+//
+//   if(message.length < 50) {
+//     statusElem.append('<div>Message is valid')
+//   } else {
+//     statusElem.append('<div>Message is too short')
+//   };
+// });
