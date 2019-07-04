6a3452523e10fc74dd256527e96d43439202c7c3
Matt Frazier <frazier3388@yahoo.com>
2019-07-04 14:08:41 +0900

    email form working

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,7 +3,6 @@ console.log('JS is working!')
 
 $(document).ready(function() {
   $('.contact-button').click(function(event) {
-    event.preventDefault()
     console.log('Button clicked')
 
     var email = $('.email').val()
@@ -11,24 +10,42 @@ $(document).ready(function() {
     var message = $('.message').val()
     var statusElem = $('.status')
     statusElem.empty()
+
 // ADD SUCCESS MESSAGE IF SENT
-    if(email.includes('@') && email.includes('.')) {
-      // statusElem.append('<div class="error-msg">Invalid Email</div>')
+
+  // let contactStatus = []
+  // function checkContactStatus(contactStatus) {
+  //   if(contactStatus != null && !contactStatus.includes(false)) {
+  //     return true
+  //   };
+  // };
+
+  // if(email != (/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)) {
+    if(email.length > 5 && email.includes('@') && email.includes('.')) {
+      // statusElem.append('<div class="success-msg">Thank you for your message! I\'ll be in concact soon!</div>')
     } else {
+      event.preventDefault()
       statusElem.append('<div class="error-msg">Invalid email</div>')
     };
 
     if(subject.length < 1) {
+      event.preventDefault()
       statusElem.append('<div class="error-msg">Subject cannot be empty</div>')
     // } else {
-    //   statusElem.append('<div class="error-msg">Subject cannot be empty</div>')
+    //   statusElem.append('<div class="success-msg">Thank you for your message! I\'ll be in concact soon!</div>')
     };
 
     if(message.length < 50) {
+      event.preventDefault()
       statusElem.append('<div class="error-msg">Message is too short')
     // } else {
-    //   statusElem.append('<div class="error-msg">Message is too short')
+    //   statusElem.append('<div class="success-msg">Thank you for your message! I\'ll be in concact soon!</div>')
     };
+
+    // if(contactStatus.every(checkContactStatus)) {
+    //   statusElem.append('<div class="success-msg">Thank you for your message! I\'ll be in concact soon!</div>')
+    // };
+
 
     // setTimeout(function() {
     //   $('.error-msg').remove();
@@ -36,35 +53,27 @@ $(document).ready(function() {
   });
 });
 
-// const button = document.querySelector('.contact-button');
-//
-// button.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   console.log('Button clicked');
 
-  // const email = ('.email').value;
-  // const subject = ('.subject').value;
-  // const message = ('.message').value;
-  // const statusElem = function {
-  //   ('.status');
-  // };
-  // statusElem.remove();
 
+// var validEmail = $(function() {
 //   if(email.includes('@') && email.includes('.')) {
-//     statusElem.append('<div>Email valid</div>')
 //   } else {
-//     statusElem.append('<div>Invalid email</div>')
-//   };
-//
-//   if(subject.null) {
-//     statusElem.append('<div>Subject is valid</div>')
-//   } else {
-//     statusElem.append('<div>Subject cannot be empty</div>')
-//   };
-//
-//   if(message.length < 50) {
-//     statusElem.append('<div>Message is valid')
-//   } else {
-//     statusElem.append('<div>Message is too short')
+//     statusElem.append('<div class="error-msg">Invalid email</div>')
 //   };
 // });
+//
+// var validSubject = $(function() {
+//   if(subject.length < 1) {
+//     statusElem.append('<div class="error-msg">Subject cannot be empty</div>')
+//   };
+// });
+//
+// var validMessage = $(function() {
+//   if(message.length < 50) {
+//     statusElem.append('<div class="error-msg">Message is too short')
+//   };
+// });
+//
+// if(validEmail == false && validSubject == false && validMessage == false) {
+//   statusElem.append('<div class="success-msg">Thank you for your message! I\'ll be in concact soon!')
+// };
